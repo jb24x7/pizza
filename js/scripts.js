e583e99c4a67f870b1ec0fecaba6b93d2c962d4c
James Provance <jb24x7@gmail.com>
2023-01-27 11:34:03 -0800

    (wip) taking user imput and adding it into pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function Pizza(pizza) {
 };
 
 
-Pizza.prototype.totalPrice = function () {
+Pizza.prototype.price = function () {
   let cost = 0;
   if (this.size === 'small') {
     cost = 6 + (this.toppings.length * 0.5)
@@ -18,3 +18,17 @@ Pizza.prototype.totalPrice = function () {
   this.price = cost
 };
 
+Pizza.prototype.toppings = function () {
+  topping.forEach(element => {
+    i = 0;
+    if(!topping[i] === 'no') {
+      this.toppings.push(topping[i]);
+    };
+  });
+};
+
+let t1 = document.getElementById("toppings1");
+let t2 = document.getElementById("toppings2");
+let t3 = document.getElementById("toppings3");
+let t4 = document.getElementById("toppings4");
+let topping = [t1[t1.selectedIndex],t2[t2.selectedIndex],t3[t3.selectedIndex],t4[t4.selectedIndex]]
