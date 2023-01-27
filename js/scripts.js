ca8ea185990bc05a2f1f221546748eb46f1fdcf7
James Provance <jb24x7@gmail.com>
2023-01-27 09:54:03 -0800

    (WIP) adding price genertaing function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 
-function pizza(name) {
-  this.name = name;
-  this.toppings = '';
+function Pizza(pizza) {
+  this.pizza = pizza;
+  this.toppings = [];
   this.size = '';
-}
+  this.price = 0;
+};
+
+
+Pizza.prototype.totalPrice = function() {
+  let cost = 0;
+  if(this.size === 'small') {
+
+  } else if(this.size === 'medium') {
+
+  } else {
+
+  };
+};
